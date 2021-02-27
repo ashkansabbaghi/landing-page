deaac1100a934c14728ba1440483ff57d1b53f29
ashakn sabbaghi <ashkansabbaghi73@gmail.com>
2021-02-27 16:26:34 +0330

    add first file

---
+++ apps/foods/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.utils.translation import gettext as _
+
+# Create your models here.
+
+class food (models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(_("توضیحات"), max_length=50)
+    photo = models.ImageField(upload_to = "foods/")
+    rate = models.IntegerField(_("اعتبار"))
+
+
+    def __str__(self):
+        return self.name , self.rate , self.description
+
